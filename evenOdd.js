3fcf7594035681a6aca061bf42c4e30488dfd5cb
pallaviparteti <pallaviparteti2000@gmail.com>
2023-09-05 18:09:31 +0530

    [pallavi]Added : code to even odd number

---
+++ evenOdd.js
@@ -0,0 +1,12 @@
+function isEvenOrOdd(number) {
+    if (number % 2 === 0) {
+      return "given number is Even number";
+    } else {
+      return "gieven number is Odd  number";
+    }
+  }
+  console.log(isEvenOrOdd(4)); 
+  console.log(isEvenOrOdd(7));  
+  console.log(isEvenOrOdd(0));  
+  console.log(isEvenOrOdd(-5));
+  
