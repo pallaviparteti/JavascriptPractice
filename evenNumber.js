008358a74791b6bbec6d4c6d2ad99585ce71b68b
pallaviparteti <pallaviparteti2000@gmail.com>
2023-09-06 16:19:57 +0530

    [pallavi]Add : even numbers beetweeen 0 to 100

---
+++ evenNumber.js
@@ -0,0 +1,10 @@
+function evenNumbers() {
+  for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+      console.log(i, "is even number");
+    } else {
+      console.log(i, "is odd number");
+    }
+  }
+}
+evenNumbers();
