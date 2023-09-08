7f2ae6d74452cca15f0e2423773a4300ddf4c253
pallaviparteti <pallaviparteti2000@gmail.com>
2023-09-08 15:41:19 +0530

    [pallavi]Add : code to find the index of all elements in array

---
+++ findIndex.js
@@ -0,0 +1,7 @@
+// Find the index of a specific element in an array.
+const array = [10, 20, 30, 40, 50, 60];
+const newArray = [];
+array.forEach((val)=>{
+    console.log(val," -----> ", array.indexOf(val))
+
+})
