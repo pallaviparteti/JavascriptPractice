8a3bed72acc107eedc1c29dd3767d529bf63e237
pallaviparteti <pallaviparteti2000@gmail.com>
2023-09-14 16:22:20 +0530

    [pallavi]Added : merge two sorted array

---
+++ mergeArray.js
@@ -0,0 +1,5 @@
+//Merge two sorted arrays into a single sorted array.
+const array = ['a','b','c','d','e']
+const array2 = ['f','g','h','i','j']
+const array3 = [...array,...array2];
+console.log(array3);
