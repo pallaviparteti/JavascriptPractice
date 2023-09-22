8d6edb872f86c139dd0162111293cbdd82a76b44
pallaviparteti <pallaviparteti2000@gmail.com>
2023-09-22 11:31:00 +0530

    [pallavi]Added : code to capital the last charecter of each word in given sentence

--- capitalStartWord.js
+++ capitalStartWord.js
@@ -1,7 +1,9 @@
-let str = " today is tuesday";
-let words = str.split('')
-for(let i =0 ; i < words.length; i++){
-    words[i] = words[i].charAt(0).toUpperCase() 
-}
-let capitalWords = words.join('');
-console.log(capitalWords);
+let str = "today is tuesday";
+let array = [];
+let words = str.split(' ')
+console.log(words)
+ const last = words.map((word)=>{
+  const lastchar = word.charAt(word.length-1)
+  return word.slice(0,-1) + lastchar.toUpperCase();
+ })
+ console.log(last.join(" "));
